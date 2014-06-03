28b8e71fa8210edb3b73fffd61e36e276323b3e6
botelhojp <botelhojp@gmail.com>
2014-06-03 12:07:37 -0300

    grafico quase ok

--- impl/gui/src/main/java/lemas/agent/AgentLoader.java
+++ impl/gui/src/main/java/lemas/agent/AgentLoader.java
@@ -3,6 +3,8 @@ package lemas.agent;
 import jade.core.AID;
 import jade.lang.acl.ACLMessage;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -20,8 +22,8 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.xy.DefaultXYDataset;
-import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 public class AgentLoader extends OpenAgent {
 
@@ -29,9 +31,10 @@ public class AgentLoader extends OpenAgent {
 	private Set<AID> wait = new HashSet<AID>();
 	private double countTrue = 0;
 	private double countFalse = 0;
-	private DefaultXYDataset ds = null;
 	private JFrame frame = null;
 	private ChartPanel cp = null;
+	private XYSeriesCollection xyDataset;
+	private long count = 0;
 
 	@Override
 	protected void setup() {
@@ -59,41 +62,69 @@ public class AgentLoader extends OpenAgent {
 		} else {
 			countFalse = countFalse + 1;
 		}
-
 		// JFrame frame = new JFrame("Charts");
-		//
 		// frame.setSize(600, 400);
 		// frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// frame.setVisible(true);
 
-		if (ds == null) {
-			//JInternalFrame frame = FrameMain.getInstance().getFrameResult();
+		if (cp == null) {
+			// JInternalFrame frame = FrameMain.getInstance().getFrameResult();
 			frame = new JFrame("Charts");
 			frame.setSize(600, 400);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.setVisible(true);
-			ds = createDataset();
-			JFreeChart chart = ChartFactory.createXYLineChart("Test Chart", "x", "y", ds, PlotOrientation.VERTICAL, true, true, false);
+			// ds = createDataset();
+			xyDataset = getAllSeries();
+			JFreeChart chart = ChartFactory.createXYLineChart("Test Chart", "x", "y", xyDataset, PlotOrientation.VERTICAL, true, true, false);
 			cp = new ChartPanel(chart);
+			JInternalFrame wd = FrameMain.getInstance().getWindow();
+			wd.setSize(600, 400);
+			wd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			wd.setVisible(true);
+			wd.add(cp);
+			
 			frame.getContentPane().add(cp);
 			cp.repaint();
-		}else{
-			cp.repaint();
+			wd.setSize(frame.getSize().width + 1, frame.getSize().height + 1);
+			frame.setSize(frame.getSize().width + 1, frame.getSize().height + 1);
+			
+		} else {
+
+			if (count < 200) {
+				count++;
+				xyDataset.getSeries(0).add(count, Math.pow(2, count) * 10);
+				xyDataset.getSeries(1).add(count, Math.pow(2, count) * 5);
+//				cp.repaint();
+//				frame.repaint();
+				
+			}
 		}
 
 		updateScree();
 	}
 
-	private static DefaultXYDataset createDataset() {
-
-		DefaultXYDataset ds = new DefaultXYDataset();
-
-		double[][] data = { { 0.1, 0.2, 0.3 }, { 1, 2, 3 } };
-
-		ds.addSeries("series1", data);
-
-		return ds;
+	protected XYSeriesCollection getAllSeries() {
+		XYSeriesCollection col = new XYSeriesCollection();
+		Collection<XYSeries> values = createSeries();
+		for (XYSeries series : values) {
+			col.addSeries(series);
+		}
+		return col;
 	}
+
+	private Collection<XYSeries> createSeries() {
+		Collection<XYSeries> series = new ArrayList<XYSeries>();
+		series.add(new XYSeries("Erro"));
+		series.add(new XYSeries("Acerto"));
+		return series;
+	}
+
+	// private static DefaultXYDataset createDataset() {
+	// DefaultXYDataset ds = new DefaultXYDataset();
+	// double[][] data = { { 0.1, 0.2, 0.3 }, { 1, 2, 3 } };
+	// ds.addSeries("series1", data);
+	// return ds;
+	// }
 
 	private void updateScree() {
 		double total = (countFalse + countTrue);
