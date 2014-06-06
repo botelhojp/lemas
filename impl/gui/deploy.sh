76f22a8a37e1201603d421239f18d2c83c7c4af5
botelhojp <botelhojp@gmail.com>
2014-06-05 21:50:20 -0300

    lixo

--- impl/gui/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo 'CRONOS'
-mvn package
-
-#keytool -genkey -keystore cronosKeys -alias jdc
-#jarsigner -keystore cronosKeys target/cronos-1.0.0-standalone.jar jdc
-
-echo 'SCP'
-scp target/cronos-1.0.0-standalone.jar vanderson@hercules.ppgia.pucpr.br:/home2/doutorado/2013/vanderson/public_html/cronos/cronos.jar
